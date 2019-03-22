ae8018f1e7f6111a0ee02b60081b23d68b044092
Nikita Levkovich <mikita.liaukovich@gmail.com>
2019-03-23 01:05:40 +0300

    Final version

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,50 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.parentNodes = [];
+		this.root = null;
 	}
 
 	push(data, priority) {
-		
+		let h = new Node(data, priority);
+		this.insertNode(h);
+		this.shiftNodeUp(h);
 	}
 
 	pop() {
-		
+
 	}
 
 	detachRoot() {
-		
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+
 	}
 
 	isEmpty() {
-		
+
 	}
 
 	clear() {
-		
+
 	}
 
 	insertNode(node) {
-		
+
 	}
 
 	shiftNodeUp(node) {
-		
+
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,22 +1,31 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if ((this.left)&&(this.right)) return
+			else if (this.left) this.right = node
+				else this.left = node;
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) this.left = null
+			else if (this.right === node) this.right = null
+				else throw new Error();
+		this.parent = null;
 	}
 
 	remove() {
-
+		if (this.parent === null) return;
 	}
 
 	swapWithParent() {
-		
+
 	}
 }
 
